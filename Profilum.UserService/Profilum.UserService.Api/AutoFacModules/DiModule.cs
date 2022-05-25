0e7e667085b829bbeab254801c4a37159c4800c3
SergheyUmca <sergheicucheryavii@gmail.com>
2022-05-25 18:43:05 +0500

    Add ignored files

---
+++ Profilum.UserService/Profilum.UserService.Api/AutoFacModules/DiModule.cs
@@ -0,0 +1,31 @@
+﻿using Autofac;
+using Autofac.Core;
+using Profilum.UserService.BLL.Handlers.Implementations;
+using Profilum.UserService.BLL.Handlers.Interfaces;
+using Profilum.UserService.Common.BaseModels;
+
+namespace Profilum.UserService.Api.AutoFacModules
+{
+    // ReSharper disable once ClassNeverInstantiated.Global
+    public class DiModule : ConfiguredModule
+    {
+        public DiModule(AppSettings settings) : base(settings)
+        {
+            
+        }
+        protected override void Load(ContainerBuilder builder)
+        {
+            var parameters = new List<Parameter>
+            {
+                new NamedParameter("connectionString", Settings.ConnectionString),
+                new NamedParameter("dbName", Settings.Database)
+            };
+            
+            builder.RegisterType<Services.UserService>().As<UserService.UserServiceBase>();
+            builder.RegisterType<UserHandler>().As<IUserHandler>().WithParameters(parameters);
+            builder.RegisterType<GrpcServer>().As<IHostedService>().InstancePerDependency().WithParameter( new NamedParameter("settings", Settings));
+        }
+
+        
+    }
+}
---
+++ Profilum.UserService/Profilum.UserService.Api/GrpcServer.cs
@@ -0,0 +1,47 @@
+﻿using Grpc.Core;
+using Profilum.UserService.Common.BaseModels;
+using UserGrpcService = Profilum.UserService.Api.Services.UserService;
+
+namespace Profilum.UserService.Api;
+
+internal class GrpcServer : IHostedService
+{
+    private readonly Server _server;
+    //private readonly UserGrpcService _userService;
+
+    public GrpcServer(UserService.UserServiceBase userService, AppSettings settings)
+    {
+        //var credentials = BuildSSLCredentials(); // строим креды из сертификата и приватного ключа. 
+        _server = new Server //создаем объект сервера
+        {
+            Ports = { new ServerPort("localhost", settings.GrpcServerPort, ServerCredentials.Insecure) }, // биндим сервер к адресу и порту
+            Services = // прописываем службы которые будут доступны на сервере
+            {
+                UserService.BindService(userService)
+            }
+        };            
+    }
+
+    /// <summary>
+    /// Вспомогательный метод генерации серверных кредов из сертификата
+    /// </summary>
+    private ServerCredentials BuildSSLCredentials()
+    {
+        var cert = File.ReadAllText("cert\\server.crt");
+        var key = File.ReadAllText("cert\\server.key");
+
+        var keyCertPair = new KeyCertificatePair(cert, key);
+        return new SslServerCredentials(new[] { keyCertPair });
+    }
+
+    public Task StartAsync(CancellationToken cancellationToken)
+    {
+        _server.Start();
+        return Task.CompletedTask;
+    }
+
+    public async Task StopAsync(CancellationToken cancellationToken)
+    {
+        await _server.ShutdownAsync();
+    }
+}
